0f52a90daebc86db8c4711661bed8e7d0bc2fbc3
Lin Shan <LinShanify@gmail.com>
2020-10-30 13:18:24 +0800

    add database and admin page

--- django_template/blog/views.py
+++ django_template/blog/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author':'Lin Shan',
-        'title': 'Blog Post 1',
-        'content': 'First Post Content',
-        'date_posted': '29/10/2020'
-    },
-    {
-        'author':'LS',
-        'title': 'Blog Post 2',
-        'content': 'Second Post Content',
-        'date_posted': '30/10/2020'
-    }
-]
+from .models import Post
 
 # Create your views here.
 def home(request):
     context = {
-        'title':'home',
-        'posts':posts,
+        'posts':Post.objects.all()
     }
     return render(request,'blog/home.html', context)
 
